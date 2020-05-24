fd471b2ae43dee6420c6973f5b006fa09d59ccc7
Aung Phyo <ahphyo@gmail.com>
2020-05-24 12:58:17 +0100

    2nd commit

--- script.py
+++ script.py
@@ -1,22 +1,22 @@
-import sys
-from os import rename
+# import sys
+# from os import rename
 
 import requests
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
-def greet(who_to_greet):
-    greeting = "Hello, {}".format(who_to_greet)
-    return greeting
+# def greet(who_to_greet):
+#     greeting = "Hello, {}".format(who_to_greet)
+#     return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
 
-print(greet('World'))
-print(greet('Aung'))
+# print(greet('World'))
+# print(greet('Aung'))
 
 # name = input("Your name? ")
 # print('Hello,', name)
